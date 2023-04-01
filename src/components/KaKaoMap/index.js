c900bbefa76e8383999b44e41ed1d2e8d01deeeb
jungdadaeng <jungdayoonyi@gmail.com>
2023-04-01 18:46:47 +0900

    위치 찾기

--- src/components/KaKaoMap/index.js
+++ src/components/KaKaoMap/index.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { Helmet } from "react-helmet";
-
+import { markerdata } from "./data/markerData";
 
 const KaKaoMap = () => {
 /*global kakao*/
@@ -9,26 +9,92 @@ useEffect(() => {
       const container = document.getElementById('map'),
         options = {
           center: new kakao.maps.LatLng(37.566826, 126.9786567), // 위도, 경도 입력
-          level: 3,
+          level: 5,
           scrollwheel: true,
           draggable: true,
         };
       const map = new kakao.maps.Map(container, options); // 지도그리기
       const mapTypeControl = new kakao.maps.MapTypeControl();
+      var markerPosition  = new kakao.maps.LatLng(37.566826, 126.9786567); 
+
+      // 마커를 생성합니다
+      var marker = new kakao.maps.Marker({
+          position: markerPosition
+      });
+
+      // 마커가 지도 위에 표시되도록 설정합니다
+      marker.setMap(map);
+
+      // markerdata.forEach((el)=>{
+      //   new kakao.maps.Marker({
+      //     //마커가 표시 될 지도
+      //     map: map,
+      //     //마커가 표시 될 위치
+      //     position: new kakao.maps.LatLng(el.lat, el.lng),
+      //     //마커에 hover시 나타날 title
+      //     title: el.title,
+      //   });
+      // })
     });
   });
 
-   const mapStyle = {
-    width: "100%",
-    height: "1000px"
-   }
+  const Wrapper = {
+    flex: "column"
+  }
+
+  const mapStyle = {
+    width: "80%",
+    height: "1000px",
+  }
+
+  const onClickMarker = (event, id) => {
+    console.log(id)
+    let lat = '';
+    let lng = '';
+     markerdata.map((data)=>{
+      if(data.id === id){
+        lat = data.lat;
+        lng = data.lng;
+      }
+    })
+    console.log(lat, lng)
+    const container = document.getElementById('map'),
+    options = {
+      center: new kakao.maps.LatLng(lat, lng), // 위도, 경도 입력
+      level: 3,
+      scrollwheel: true,
+      draggable: true,
+    };
+    const map = new kakao.maps.Map(container, options); // 지도그리기
+    var markerPosition  = new kakao.maps.LatLng(lat, lng); 
+
+    // 마커를 생성합니다
+    var marker = new kakao.maps.Marker({
+        position: markerPosition
+    });
+
+    // 마커가 지도 위에 표시되도록 설정합니다
+    marker.setMap(map);
+   
+  }
+
     return(
         <>
         <Helmet>
               <script src="//dapi.kakao.com/v2/maps/sdk.js?appkey=1ea25ee84af7205c783789283935a024" type="text/javascript" />
         </Helmet>
+        <div style={Wrapper}>
             <h1>카카오맵 지도 연동</h1>
-            <div id="map" style={mapStyle}></div>
+            <h1>위치 찾기 :<input type="text"></input></h1>
+            <ol>
+              {markerdata.map((data)=>{
+                return ( <li key={data.id}><button onClick={(e)=>onClickMarker(e, data.id)} >{data.title}</button></li>)
+              })}
+              
+            </ol>
+            <div id="map" style={mapStyle}>
+            </div>
+         </div>
         </>
     )
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,8 @@
 import './page.css'
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Helmet } from 'react-helmet';
 import { useNavigate } from "react-router-dom";
-
+// const { kakao } = window;
 const Login = () => {
     const navigate = useNavigate();
     const [id, setId] = useState("");
@@ -28,8 +29,32 @@ const Login = () => {
 
         navigate("/");
     };
+    window.Kakao.init('1ea25ee84af7205c783789283935a024');
+
+        function kakaoLogin() {
+            window.Kakao.Auth.login({
+                scope: 'nickname', //동의항목 페이지에 있는 개인정보 보호 테이블의 활성화된 ID값을 넣습니다.
+                success: function(response) {
+                    console.log(response) // 로그인 성공하면 받아오는 데이터
+                    window.Kakao.API.request({ // 사용자 정보 가져오기 
+                        url: '/v2/user/me',
+                        success: (res) => {
+                            const kakao_account = res.kakao_account;
+                            console.log(kakao_account)
+                        }
+                    });
+                    navigate("/"); //리다이렉트 되는 코드
+                },
+                fail: function(error) {
+                    console.log(error);
+                }
+            });
+        }
     return (
         <div>
+            <Helmet>
+              <script src="//dapi.kakao.com/v2/maps/sdk.js?appkey=1ea25ee84af7205c783789283935a024" type="text/javascript" />
+            </Helmet>
             <h3 className="name">Login</h3>
             <form className="login-form" method="post" onSubmit={handleLogin}>
                 <label className="legend">아이디</label>
@@ -40,6 +65,12 @@ const Login = () => {
                 <div className="find-btn">
                     <button type="submit" className="btn btn-navy navbar-btn find-btn1">로그인</button>
                 </div>
+                    <button onClick="kakaoLogin">
+                        <span>카카오 로그인</span>
+                    </button>
+                    <button onClick="kakaoLogout">
+                        <span>카카오 로그아웃</span>
+                    </button>
             </form>
         </div>
     )
